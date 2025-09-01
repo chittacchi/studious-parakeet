a2290d8ab7b4abc5c2b0cd22ca2c4bc9bf3f367d
chittacchi <flufluflum@gmail.com>
2025-09-01 14:22:32 +0900

    フォルダをアップロード
    参考のコード

---
+++ answer/main.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('.modal-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.modal-close');
+
+button.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
